6f4619cdee16f4506d102667dcd5153beef56beb
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-08 09:24:42 -0600

    test/relationships ans set up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
-turing_garden = Garden.create!(name: 'Turing Community Garden', organic: true)
+turing_garden_1 = Garden.create!(name: 'Turing Community Garden', organic: true)
 library_garden = Garden.create!(name: 'Public Library Garden', organic: true)
 other_garden = Garden.create!(name: 'Main Street Garden', organic: false)
 
-turing_garden.plots.create!(number: 25, size: "Large", direction: "East")
-turing_garden.plots.create!(number: 26, size: "Small", direction: "West")
-library_garden.plots.create!(number: 2, size: "Small", direction: "South")
-other_garden.plots.create!(number: 738, size: "Medium", direction: "West")
+turing_plot_1 = Plot.create!(number: 25, size: "Large", direction: "East")
+turing_plot_2 = Plot.create!(number: 26, size: "Small", direction: "West")
+library_plot = Plot.create!(number: 2, size: "Small", direction: "South")
+other_plot = Plot.create!(number: 738, size: "Medium", direction: "West")
+
+tomato = Plant.create!(name: 'Potatoe', description: 'Russet', days_to_harvest: 95)
+bell_pepper = Plant.create!(name: 'Bell Pepper', description: 'Red', days_to_harvest: 90)
+squash = Plant.create!(name: 'Squash', description: 'Patty Pan', days_to_harvest: 60)
+garlic = Plant.create!(name: 'Garlic', description: 'Bulb', days_to_harvest: 120)
+cucumber = Plant.create!(name: 'Cucumber', description: 'Yellow', days_to_harvest: 101)
+

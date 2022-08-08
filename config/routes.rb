7a3615858502e8925446ea26fd4bc3c167ce69ec
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-08 10:41:31 -0600

    feat/Click button to remove plant plot association

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :plots
+
+  delete '/plot_plants/:plot_id/:plant_id', to: "plot_plants#destroy"
 end
